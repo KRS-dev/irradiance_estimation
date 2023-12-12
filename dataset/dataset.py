557d4cc30b4ec5db7b5d6294f921742c96748be3
Kevin Schuurman <kschuurm@daint104.login.cscs.ch>
2023-12-12 16:45:48 +0100

    fixed point dataset, added simple minmax tranform class

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -22,6 +22,21 @@ import torch
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 
+
+MINMAX = {'SIS': (0.0, 1109.0), 
+          'SRTM': (-7.6700854700854695, 3746.053675213676), 
+          'channel_1': (-0.09116022288799286, 18.401456832885742), 
+          'channel_10': (13.867655754089355, 177.00460815429688), 
+          'channel_11': (20.31503677368164, 117.05536651611328), 
+          'channel_2': (-0.17268221080303192, 21.99477767944336), 
+          'channel_3': (-0.2678762376308441, 13.346491813659668), 
+          'channel_4': (-0.008304266259074211, 2.9166910648345947), 
+          'channel_5': (0.44394731521606445, 6.937240123748779), 
+          'channel_6': (1.3654084205627441, 23.604351043701172), 
+          'channel_7': (3.807188034057617, 108.10502624511719), 
+          'channel_8': (9.00473403930664, 74.256103515625), 
+          'channel_9': (10.230449676513672, 165.13319396972656)}
+
 def pyephem(datetimes, latitude, longitude, altitude=0, pressure=101325,
             temperature=12, horizon='+0:00', rad=True):
     """
@@ -70,11 +85,9 @@ def pyephem(datetimes, latitude, longitude, altitude=0, pressure=101325,
     # sun_coords = pd.DataFrame(index=time)
         
     obs = ephem.Observer()
-    print(latitude, longitude, altitude)
     obs.lat = str(latitude)
     obs.lon = str(longitude)
     obs.elevation = altitude if not np.isnan(altitude) else 0
-    print(obs)
     sun = ephem.Sun()
 
 
@@ -108,56 +121,19 @@ def solarzenithangle(datetime, lat, lon, alt):
 
 
 class MSGDataset(Dataset):
-    def __init__(self, dataset, y_var, x_vars=None, rechunk=None):
-
+    def __init__(self, dataset, y_var, x_vars=None, transform=None, target_transform=None):
         if isinstance(dataset, str):
             self.ds = xarray.open_zarr(dataset)
         elif isinstance(dataset, xarray.Dataset):
             self.ds = dataset
         else:
             raise ValueError(f'{dataset} is not a zarr store or a xarray.Dataset.')
-            
+        
+        self.ds = self.ds.drop_vars(('lat_bnds','lon_bnds'))
         self.attributes = self.ds.attrs
 
-        vars = set(self.ds.keys())
-        ignore_vars = set(['lon_bnds', 'lat_bnds', 'record_status', 'crs'])
-        assert y_var in vars, 'y_var not available in the dataset'
-        self.y_var = y_var
-        self.x_vars = vars - ignore_vars - set([y_var,])
-
-    def __len__(self):
-        return len(self.ds.time)
-
-    def __getitem__(self, idx):
-        if self.ds is not None:
-            xarr = self.ds[self.x_vars].isel(time=idx).to_dataarray(dim='channels') # select time slice, reshape to t lon lat
-            yarr = self.ds[self.y_var].isel(time=idx)
-            x = torch.tensor(data=xarr.data, names=('C', 'T', 'Y', 'X'))
-            y = torch.tensor(data=yarr.data, names=('C', 'T', 'Y', 'X'))      
-            return x, y
-        else:
-            raise ValueError('There is no zarr dataset to get images from.')
-        
-class MSGDatasetBatched(Dataset):
-    def __init__(self, zarr_store, 
-                 y_var =None, 
-                 x_vars=None, 
-                 patch_size = (64,64), 
-                 batch_size = 10,
-                 x_features_bool = False, 
-                 transform=None, 
-                 target_transform=None):
-
-        self.ds = xarray.open_zarr(zarr_store, ).drop_vars(('lat_bnds','lon_bnds'))
-        self.patch_size = patch_size
-        self.batch_size = batch_size
-        self.generator = BatchGenerator(self.ds, 
-                                        input_dims={'lat':self.patch_size[1], 'lon':self.patch_size[0]}, 
-                                        batch_dims={'time':self.batch_size})
         self.transform = transform
         self.target_transform = target_transform
-        self.attrs = self.ds.attrs
-        self.x_features_bool = x_features_bool
 
         vars = set(self.ds.keys())
         assert y_var in vars, 'y_var not available in the dataset'
@@ -170,6 +146,42 @@ class MSGDatasetBatched(Dataset):
             self.x_vars = x_vars
         else:
             self.x_vars = other_vars
+
+    def __len__(self):
+        return len(self.ds.time)
+
+    def __getitem__(self, idx):
+        if self.ds is not None:
+            xarr = self.ds[self.x_vars].isel(time=idx)
+            if self.transform:
+                xarr = self.transform(xarr)
+            xarr = xarr.to_dataarray() # select time slice, reshape to t lon lat
+            
+            yarr = self.ds[self.y_var].isel(time=idx)
+            x = torch.tensor(data=xarr.values).permute(1,0,2,3) # (B, C, X, Y)
+            y = torch.tensor(data=yarr.values) # (B, X, Y)
+            return x, y
+        else:
+            raise ValueError('There is no zarr dataset to get images from.')
+        
+class MSGDatasetBatched(MSGDataset):
+    def __init__(self, zarr_store, 
+                 y_var =None, 
+                 x_vars=None, 
+                 patch_size = (64,64), 
+                 batch_size = 10,
+                 x_features_bool = False, 
+                 transform=None, 
+                 target_transform=None):
+        super().__init__(zarr_store, y_var, x_vars, transform=transform, target_transform=target_transform)
+
+        self.patch_size = patch_size
+        self.batch_size = batch_size
+        self.generator = BatchGenerator(self.ds, 
+                                        input_dims={'lat':self.patch_size[1], 'lon':self.patch_size[0]}, 
+                                        batch_dims={'time':self.batch_size})
+        
+        self.x_features_bool = x_features_bool
 
     def __len__(self) -> int:
         return len(self.generator)
@@ -185,59 +197,17 @@ class MSGDatasetBatched(Dataset):
                 )
 
         X_batch_ds, y_batch_ds = self.get_dataset_batch(idx)
-
-        if self.x_features_bool:            
-            x_batch_attributes = self.get_extra_features(X_batch_ds) 
-            x_batch_attributes = torch.tensor(x_batch_attributes)
         
         X_batch_ds = X_batch_ds.fillna(-99999) # for training we do not want any nans. Very negative values should cancel with ReLU 
         
         X_batch = torch.tensor(X_batch_ds.to_dataarray(dim='channels').values)#, names=('B','C','T', 'X', 'Y')) 
         y_batch = torch.tensor(y_batch_ds.to_dataarray(dim='channels').values)#, names=('B', 'T', 'X', 'Y'))  
         
-        X_batch = X_batch.squeeze().permute(1,0,2,3) #Squeeze out time dimension 
-        y_batch = y_batch.squeeze() #.squeeze(-2).unsqueeze(1) (B, T, X, Y) -> (B, C, X, Y) where dim(C)=1
-        if self.transform:
-            X_batch = self.transform(X_batch)
-
-        if self.target_transform:
-            y_batch = self.target_transform(y_batch)
+        X_batch = X_batch.permute(1,0,2,3) #Squeeze out time dimension 
+        y_batch = y_batch #.squeeze(-2).unsqueeze(1) (B, T, X, Y) -> (B, C, X, Y) where dim(C)=1
         
         
-        if self.x_features_bool:
-            return X_batch, x_batch_attributes, y_batch
-        else:
-            return X_batch, y_batch # ('B', 'C', 'X', 'Y') &  ('B', 'X', 'Y')
-    
-    def get_extra_features(self, X_batch_ds):
-        if self.patch_size[0] % 2 == 0:
-                idx = (int(self.patch_size[0]/2 -1), int(self.patch_size[0]/2))
-                features = X_batch_ds.isel(lat=slice(idx[0], idx[1]+1), lon=slice(idx[0], idx[1]+1))
-                lat = features.lat.mean().item()
-                lon = features.lon.mean().item()
-                features = features.mean(dim=('lat','lon'))
-        else:
-            idx = int(self.patch_size[0]/2)
-            features = X_batch_ds.isel(lat=idx, lon=idx)
-            lat = features.lat.item()
-            lon = features.lon.item()
-
-        if 'SRTM' in set(X_batch_ds.keys()):
-            srtm = features.SRTM.item()
-        else:
-            srtm = self.ds.SRTM.sel(lat=lat, lon=lon, method='nearest')     
-        datetimes = pd.to_datetime(features.time)
-        month = features.time.dt.month.values
-        day = features.time.dt.day.values
-        sza = solarzenithangle(datetimes, lat, lon, srtm if not np.isnan(srtm) else 0)  # Assume altitude is zero at nans for sza calc
-        
-        srtm = srtm if not np.isnan(srtm) else -99999 # Very small number for ML algorithm to cancel out
-        b = np.repeat(np.array([lat,lon,srtm]).reshape(1,-1), self.batch_size,axis=0)
-        arr = np.hstack([month.reshape(-1,1), day.reshape(-1,1), sza.reshape(-1,1), b])
-
-        # sza = solarzenithangle(y_batch.time.dt, features[0], features[1], features[2])
-        x_batch_attributes = torch.tensor(arr)
-        return arr  # (B, F), F columns: [month, day, sza, lat, lon, srtm]
+        return X_batch, y_batch # ('B', 'C', 'X', 'Y') &  ('B', 'X', 'Y')
     
     def get_xarray_batch(self, idx) -> Tuple[xarray.DataArray, xarray.DataArray]:
         '''returns a batch in xarray.DataArray form'''
@@ -268,15 +238,128 @@ class MSGDatasetBatched(Dataset):
         a= self.generator[idx]
         X_batch = a[self.x_vars]
         y_batch = a[[self.y_var]]
+
+        if self.transform:
+            X_batch = self.transform(X_batch)
+
+        if self.target_transform:
+            y_batch = self.target_transform(y_batch)
+
         return X_batch, y_batch
 
 
+
+class MSGDatasetPoint(MSGDatasetBatched):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, idx) -> Tuple[Any, Any]:
+        '''returns a batch in torch tensors form'''
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+            if len(idx) == 1:
+                idx = idx[0]
+            else:
+                raise NotImplementedError(
+                    f"{type(self).__name__}.__getitem__ currently requires a single integer key"
+                )
+        
+        X_batch, x_attributes, y = self.get_xarray_batch(idx)
+        X_batch = X_batch.fillna(-99999) # for training we do not want any nans. Very negative values should cancel with ReLU 
+        
+        X_batch = torch.tensor(X_batch.values) #, names=('C', 'B', 'X', 'Y'))
+        x_attributes = torch.tensor(x_attributes.values) # ('B', 'C')
+        y = torch.tensor(y.values) #, names=('B')  
+        
+        X_batch = X_batch.permute(1,0,2,3) # ('B', 'C', 'X', 'Y')
+        x_attributes = x_attributes.permute(1,0)
+        y = y.permute(1,0)
+
+        if self.transform:
+            X_batch = self.transform(X_batch)
+
+        if self.target_transform:
+            y_batch = self.target_transform(y_batch)        
+
+        return X_batch, x_attributes, y
+
+    def get_xarray_batch(self, idx) -> Tuple[xarray.DataArray, xarray.DataArray]:
+        '''returns a batch in xarray.DataArray form'''
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+            if len(idx) == 1:
+                idx = idx[0]
+            else:
+                raise NotImplementedError(
+                    f"{type(self).__name__}.__getitem__ currently requires a single integer key"
+                )
+
+        X_batch, y_batch = self.get_dataset_batch(idx)
+        if self.patch_size[0] % 2 == 0:
+            idx = (int(self.patch_size[0]/2 -1), int(self.patch_size[0]/2))
+            y_batch_point = y_batch.isel(lat=slice(idx[0], idx[1]+1), lon=slice(idx[0], idx[1]+1)).mean(dim=('lat','lon'), keepdims=True).to_dataarray(dim='channels')
+        else:
+            idx = int(self.patch_size[0]/2)
+            y_batch_point = y_batch.isel(lat=idx, lon=idx).to_dataarray(dim='channels')
+        
+        x_attributes = self.get_point_features(X_batch).to_dataarray(dim='channels')
+        X_batch = X_batch.to_dataarray(dim='channels')
+
+        return X_batch, x_attributes, y_batch_point
+        
+    
+    def get_point_features(self, X_batch_ds ):
+        if self.patch_size[0] % 2 == 0:
+            idx = (int(self.patch_size[0]/2 -1), int(self.patch_size[0]/2))
+            features = X_batch_ds.isel(lat=slice(idx[0], idx[1]+1), lon=slice(idx[0], idx[1]+1))
+            lat = features.lat.mean().item()
+            lon = features.lon.mean().item()
+            features = features.mean(dim=('lat','lon'))
+        else:
+            idx = int(self.patch_size[0]/2)
+            features = X_batch_ds.isel(lat=idx, lon=idx)
+            lat = features.lat.item()
+            lon = features.lon.item()
+
+        if 'SRTM' in set(X_batch_ds.keys()):
+            srtm = features.SRTM.item()
+        else:
+            srtm = self.ds.SRTM.sel(lat=lat, lon=lon, method='nearest')     
+
+        datetimes = pd.to_datetime(features.time)
+        month = features.time.dt.month.values
+        day = features.time.dt.day.values
+        sza = solarzenithangle(datetimes, lat, lon, srtm if not np.isnan(srtm) else 0)  # Assume altitude is zero at nans for sza calc
+        
+        srtm = srtm if not np.isnan(srtm) else -99999 # Very small number for ML algorithm to cancel out
+        lat = np.repeat(lat, self.batch_size)
+        lon = np.repeat(lon, self.batch_size)
+        srtm = np.repeat(srtm, self.batch_size)
+
+
+        # arr = np.hstack([month.reshape(-1,1), day.reshape(-1,1), sza.reshape(-1,1), b])
+
+        features = xarray.Dataset(
+            data_vars={
+            'month':(('time'),month),
+            'day':(('time'),day),
+            'lat':(('time'), lat),
+            'lon':(('time'), lon),
+            'srtm': (('time'), srtm),
+            'sza':(('time'),sza),
+            },
+        )
+        
+        return features  # (B, F) , F columns: [month, day, sza, lat, lon, srtm]
+
 class MSGDataModule(L.LightningDataModule):
-    def __init__(self, batch_size: int = 32, patch_size: (int, int)=None, num_workers:int=12, x_vars=None):
+    def __init__(self, batch_size: int = 32, patch_size: (int, int)=None, num_workers:int=12, x_vars=None, transform=None):
         super().__init__()
         self.batch_size = batch_size
         self.patch_size = patch_size
         self.num_workers = num_workers
+
+        self.tranform = transform
         
 
     def setup(self, stage: str ):
@@ -285,7 +368,9 @@ class MSGDataModule(L.LightningDataModule):
                 # Using xbatcher to make batches of patched data
                 self.train_dataset = MSGDatasetBatched('/scratch/snx3000/kschuurm/DATA/train.zarr', 
                                                        y_var='SIS', 
-                                                       patch_size=self.patch_size, batch_size=self.batch_size)
+                                                       patch_size=self.patch_size, 
+                                                       batch_size=self.batch_size,
+                                                       transform=self.tranform)
                 self.val_dataset = MSGDatasetBatched('/scratch/snx3000/kschuurm/DATA/valid.zarr', 
                                                      y_var='SIS', patch_size=self.patch_size, 
                                                      batch_size=self.batch_size)
@@ -316,6 +401,50 @@ class MSGDataModule(L.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=None, num_workers=self.num_workers)
 
+class MSGDataModulePoint(MSGDataModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        assert self.patch_size is not None, 'To create a patch to point datamodule a patchsize is required'
+        
+
+    def setup(self, stage: str ):
+        if stage == 'fit':
+            # Using xbatcher to make batches of patched data
+            self.train_dataset = MSGDatasetPoint('/scratch/snx3000/kschuurm/DATA/train.zarr', 
+                                                    y_var='SIS', 
+                                                    patch_size=self.patch_size, batch_size=self.batch_size)
+            self.val_dataset = MSGDatasetPoint('/scratch/snx3000/kschuurm/DATA/valid.zarr', 
+                                                    y_var='SIS', patch_size=self.patch_size, 
+                                                    batch_size=self.batch_size)
+        if stage == 'test':
+            self.test_dataset = MSGDatasetBatched('/scratch/snx3000/kschuurm/DATA/validation.zarr',
+                                                    y_var='SIS', patch_size=self.patch_size, 
+                                                    batch_size=self.batch_size)
+
+class MinMax:
+    def forward_dataset(ds):
+        for var in ds.keys():
+            minvar, maxvar = MINMAX[var]
+            ds[var] = (ds[var] - minvar)/(maxvar - minvar)
+        return ds
+    
+    def backward_dataset(ds):
+        for var in ds.keys():
+            minvar, maxvar = MINMAX[var]
+            ds[var] = ds[var]*(maxvar - minvar) + minvar
+        return ds
+    
+    def forward(array, var):
+        minvar, maxvar = MINMAX[var]
+        return (array - minvar)/(maxvar - minvar)
+
+    def backward(array, var):
+        minvar, maxvar = MINMAX[var]
+        return array*(maxvar - minvar) + minvar
+
+
+
 
 
 
@@ -337,9 +466,16 @@ if __name__ == '__main__':
 
     # plt.imshow(y.view(-1, 128))
     # plt.colorbar()
-    # plt.savefig('testsamples.png')
+    # plt.savefig('testbatchs.png')
 
-    ds = MSGDatasetBatched('/scratch/snx3000/kschuurm/DATA/valid.zarr', y_var='SIS', x_features_bool=True, patch_size=(15,15))
+    ds = MSGDatasetPoint('/scratch/snx3000/kschuurm/DATA/valid.zarr', y_var='SIS', x_vars=['channel_1', 'channel_2'], x_features_bool=True, patch_size=(15,15), transform=MinMax.forward_dataset)
 
-    a = ds[0]
-    print(a)
+    X, x, y = ds[0]
+    print(X.shape, x.shape, y.shape)
+
+    MINMAX = {}
+    for var in ds.ds.keys():
+        MINMAX[var] = (ds.ds[var].min().values.item(), ds.ds[var].max().values.item())
+    
+    print(MINMAX)
+
